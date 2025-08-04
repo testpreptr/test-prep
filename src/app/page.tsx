b7dc08e2fe9d356226a29b73ddd6154f52ac84cd
tpcwebbd-ops <tpc.web.bd@example.com>
2025-08-04 16:32:31 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-
-import FooterSectionComponents from "./components/Footer";
-import HeroSection from "./components/Hero";
-import TestPrepHero from "./components/Hero2";
-import TestPrepClasses from "./components/OurClasses";
-import StudyAbroad from "./components/StudyAbroad";
-
 export default function Home() {
-  return (
-    <>
-    <HeroSection />
-    <TestPrepHero />
-    <TestPrepClasses />
-    <StudyAbroad />
-    <FooterSectionComponents />
-    </>
-  );
+  return <div>Project 1.2</div>;
 }
